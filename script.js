171e7f51e566c3af94af48f27140dbf874d7821c
Nico <rivenf@gmail.com>
2024-05-24 15:04:03 -0400

    Add form data handling

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/* Navigation dropdown */
 $("#menuIcon").click(function(){
   $(".navDropdown").toggleClass("active");
 });
+
+/* Log form values to the console */
+const form = document.getElementById("quoteForm");
+const submitter = document.getElementById("submitBtn");
+const formData = new FormData(form, submitter);
+
+function logData() {
+  console.log(formData);
+}
+
+submitBtn.addEventListener('click', logData);
